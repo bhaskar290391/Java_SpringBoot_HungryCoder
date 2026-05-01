626d8f8e28899a30faa00abf21c367c1d774baeb
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-05-01 18:44:05 +0530

    Building Rest API for books

---
+++ Bhaskar_Coding/14_SpringBoot_API_Data_Access/Books-Restful-Webservice/src/main/java/com/example/books/BooksRestfulWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksRestfulWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksRestfulWebserviceApplication.class, args);
+	}
+
+}
