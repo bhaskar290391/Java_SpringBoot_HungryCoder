a2c4ceba2a2b02b3a14f9c4c4b18e8e84bce5cd0
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-05-02 18:19:38 +0530

    Different version of Query

--- Bhaskar_Coding/14_SpringBoot_API_Data_Access/jpa-data-access/src/main/java/com/example/jpa/jpa_demo/dao/UserRepository.java
+++ Bhaskar_Coding/14_SpringBoot_API_Data_Access/jpa-data-access/src/main/java/com/example/jpa/jpa_demo/dao/UserRepository.java
@@ -1,9 +1,13 @@
 package com.example.jpa.jpa_demo.dao;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.jpa.jpa_demo.entity.User;
 
@@ -13,8 +17,24 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
 	@Query("select u from User u where u.email=?1")
 	public User findByEmailAdrress(String email);
-	
-	@Query(value = "select * from user_management_table u where u.email=:email",nativeQuery = true)
+
+	@Query(value = "select * from user_management_table u where u.email=:email", nativeQuery = true)
 	public User findByEmailAdrressByNative(String email);
+
+	@Query("select u from User u where u.firstName like %:firstName% ")
+	public List<User> findFirstNameContaining(@Param("firstName") String firstName, Sort sort);
+
+	@Query(value = "select * from user_management_table u where u.first_name like :firstName", nativeQuery = true)
+	public List<User> findFirstNameContainingNative(@Param("firstName") String firstName, Pageable page);
+	
+	
+	@Modifying
+	@Query("update User u set u.firstName=:firstName where u.id=:id")
+	public int updateUser(String firstName, long id);
+	
+	
+	@Modifying
+	@Query("delete from User u where u.id=id")
+	public int deleteUser( long id);
 
 }
