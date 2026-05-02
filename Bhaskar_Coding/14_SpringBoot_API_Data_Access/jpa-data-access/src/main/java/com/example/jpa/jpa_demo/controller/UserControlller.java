0d491b38427cf42ea2982237b87434151a27ecd2
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-05-02 17:37:49 +0530

    Entity manager codes

--- Bhaskar_Coding/14_SpringBoot_API_Data_Access/jpa-data-access/src/main/java/com/example/jpa/jpa_demo/controller/UserControlller.java
+++ Bhaskar_Coding/14_SpringBoot_API_Data_Access/jpa-data-access/src/main/java/com/example/jpa/jpa_demo/controller/UserControlller.java
@@ -2,7 +2,12 @@ package com.example.jpa.jpa_demo.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +24,38 @@ public class UserControlller {
 	public UserControlller(UserService service) {
 		super();
 		this.service = service;
+	}
+
+	@PostMapping
+	public void createUser(@RequestBody User user) {
+		service.createUser(user);
+		System.out.println("User created Successfully");
+	}
+
+	@GetMapping("/{id}")
+	public User FindUserById(@PathVariable long id) {
+		return service.findById(id);
+	}
+
+	@GetMapping()
+	public List<User> findAllUsers() {
+		return service.findAllUsers();
+	}
+
+	@GetMapping("/native/findAll")
+	public List<User> findAllUsersNative() {
+		return service.findAllUsersByNative();
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteUser(@PathVariable long id) {
+		User user = service.findById(id);
+		service.deleteByUser(user);
+	}
+	
+	@PutMapping
+	public User updateUser(@RequestBody User user ) {
+		return service.updateUser(user);
 	}
 
 	@GetMapping("/byLastName")
--- Bhaskar_Coding/14_SpringBoot_API_Data_Access/jpa-data-access/src/main/java/com/example/jpa/jpa_demo/dao/UserService.java
+++ Bhaskar_Coding/14_SpringBoot_API_Data_Access/jpa-data-access/src/main/java/com/example/jpa/jpa_demo/dao/UserService.java
@@ -3,13 +3,51 @@ package com.example.jpa.jpa_demo.dao;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.jpa.jpa_demo.entity.User;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 
 @Service
 public class UserService {
 
 	private UserRepository repository;
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	@Transactional
+	public void createUser(User user) {
+		manager.persist(user);
+	}
+
+	public User findById(long id) {
+		return manager.find(User.class, id);
+	}
+
+	@Transactional
+	public void deleteByUser(User user) {
+		manager.remove(user);
+
+	}
+
+	@Transactional
+	public User updateUser(User user) {
+		return manager.merge(user);
+	}
+
+	public List<User> findAllUsers() {
+		Query query = manager.createQuery("select u from User u");
+		return query.getResultList();
+	}
+
+	public List<User> findAllUsersByNative() {
+		Query query = manager.createNativeQuery("select * from user_management_table u", User.class);
+		return query.getResultList();
+	}
 
 	public UserService(UserRepository repository) {
 		this.repository = repository;
@@ -22,7 +60,7 @@ public class UserService {
 	public User findByEmailAddress(String email) {
 		return repository.findByEmailAdrress(email);
 	}
-	
+
 	public User findByEmailAddressByNative(String email) {
 		return repository.findByEmailAdrressByNative(email);
 	}
