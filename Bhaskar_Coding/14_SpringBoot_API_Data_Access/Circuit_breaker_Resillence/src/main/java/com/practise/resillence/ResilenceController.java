b61005325db12e846b95d971d9fe8eb4b9714b1c
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-05-02 14:59:41 +0530

    Circuit breaker pattern

--- Bhaskar_Coding/14_SpringBoot_API_Data_Access/Circuit_breaker_Resillence/src/main/java/com/practise/resillence/ResilenceController.java
+++ Bhaskar_Coding/14_SpringBoot_API_Data_Access/Circuit_breaker_Resillence/src/main/java/com/practise/resillence/ResilenceController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -13,6 +14,16 @@ public class ResilenceController {
 
 	@Autowired
 	private RestTemplate template;
+	
+	
+	@GetMapping("/circuitBreaker")
+	@CircuitBreaker(name = "circuitBreakerDemo", fallbackMethod = "cricutiBreakerResponse")
+	public String getCircuitBreakerData() {
+		System.out.println("Inside the getCircuitBreakerData !!");
+
+		String forObject = template.getForObject("http://localhost:9090/bhaskar", String.class);
+		return forObject;
+	}
 
 	@GetMapping("/retry")
 	@Retry(name = "retryApi", fallbackMethod = "retryResponse")
@@ -22,6 +33,11 @@ public class ResilenceController {
 		String forObject = template.getForObject("http://localhost:9090/bhaskar", String.class);
 		return forObject;
 	}
+	public String cricutiBreakerResponse(Exception ex) {
+		System.out.println("Inside the cricutiBreakerResponse !!!!!!!");
+	    return "API is not working for Circit breaking: " + ex.getMessage();
+	}
+	
 	public String retryResponse(Exception ex) {
 		System.out.println("Inside the retryResponse !!!!!!!");
 	    return "API is not working: " + ex.getMessage();
