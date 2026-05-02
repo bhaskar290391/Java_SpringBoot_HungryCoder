33fa7a7def5ed3d5e213f13c115addd97df32ccb
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-05-02 08:13:48 +0530

    File upload

--- Bhaskar_Coding/14_SpringBoot_API_Data_Access/Books-Restful-Webservice/src/main/java/com/example/books/controller/FileController.java
+++ Bhaskar_Coding/14_SpringBoot_API_Data_Access/Books-Restful-Webservice/src/main/java/com/example/books/controller/FileController.java
@@ -1,6 +1,10 @@
 package com.example.books.controller;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,14 +17,15 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileController {
 
 	@PostMapping("/upload")
-	public String upload(@RequestParam("file") MultipartFile file) {
-		
+	public String upload(@RequestParam MultipartFile file) {
+
 		try {
-			file.transferTo(new File("‪C:\\Users\\A_R_COMPUTERS\\Desktop\\Bhaskar_File\\" +file.getOriginalFilename()));
-			return "File Uploaded Successfully "+file.getOriginalFilename();
+			Path path = Paths.get("C:/Users/A_R_COMPUTERS/Desktop/Bhaskar_File/" + file.getOriginalFilename());
+			Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+			return "File Uploaded Successfully " + file.getOriginalFilename();
 		} catch (Exception e) {
 			e.printStackTrace();
-			return "File Upload is failed by original file" + file.getOriginalFilename(); 
+			return "File Upload is failed by original file" + file.getOriginalFilename();
 		}
 	}
 }
