a0e67203d510885f3b4bec580c7e8f13364641ba
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-05-02 07:38:33 +0530

    Get Mapping with Request Params

--- Bhaskar_Coding/14_SpringBoot_API_Data_Access/Books-Restful-Webservice/src/main/java/com/example/books/controller/BookController.java
+++ Bhaskar_Coding/14_SpringBoot_API_Data_Access/Books-Restful-Webservice/src/main/java/com/example/books/controller/BookController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.books.entity.Book;
@@ -59,5 +60,11 @@ public class BookController {
 		}
 
 		return null;
+	}
+	
+	
+	@GetMapping("/search")
+	public String getSearchBook(@RequestParam String title) {
+		return title;
 	}
 }
