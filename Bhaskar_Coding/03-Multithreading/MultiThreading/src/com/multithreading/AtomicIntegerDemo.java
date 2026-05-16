39672c7df72528176acb582dd3269ce46ad03e1b
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-05-16 19:39:43 +0530

    AtomicInteger demo

---
+++ Bhaskar_Coding/03-Multithreading/MultiThreading/src/com/multithreading/AtomicIntegerDemo.java
@@ -0,0 +1,55 @@
+package com.multithreading;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+
+class SharedData{
+
+    public AtomicInteger count=new AtomicInteger(0);
+
+    public int getCount() {
+        return count.get();
+    }
+
+    public void increment() {
+        count.incrementAndGet();
+    }
+}
+
+public class AtomicIntegerDemo {
+
+    static void main() throws InterruptedException {
+        SharedData data=new SharedData();
+       Thread t2= new Thread(()-> {
+
+            System.out.println("Thread 1 started");
+            for (int i = 0; i < 50000; i++) {
+                data.increment();
+            }
+
+            System.out.println("Thread 1 is completed ");
+        });
+
+
+
+
+   Thread t1= new Thread(()-> {
+
+        System.out.println("Thread 2 started");
+        for (int i = 0; i < 50000; i++) {
+            data.increment();
+        }
+
+        System.out.println("Thread 2 is completed ");
+    });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("The value of counter is ==>"+data.getCount());
+}
+}
