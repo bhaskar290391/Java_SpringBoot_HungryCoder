9ad6feb58cd546c1eb0e7a08a24f92e783ac3109
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-05-20 10:42:01 +0530

    my First program

---
+++ Bhaskar_Coding/27- Useful-videos/Java_Useful_vide/src/com/practise/MyFirstProgram.java
@@ -0,0 +1,9 @@
+package com.practise;
+
+public class MyFirstProgram {
+    public static void main(String... args) {
+        System.out.println("Hello World");
+        System.out.println(args.length);
+        System.out.println("The arguments is ==> "+ args[0]+" "+args[1]);
+    }
+}
