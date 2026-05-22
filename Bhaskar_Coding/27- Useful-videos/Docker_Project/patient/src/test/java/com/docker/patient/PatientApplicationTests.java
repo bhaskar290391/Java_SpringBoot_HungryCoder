02bb9725496cf442a36ac39e42ddffce431b3e75
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-05-22 11:01:52 +0530

    Patient docker service

---
+++ Bhaskar_Coding/27- Useful-videos/Docker_Project/patient/src/test/java/com/docker/patient/PatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
