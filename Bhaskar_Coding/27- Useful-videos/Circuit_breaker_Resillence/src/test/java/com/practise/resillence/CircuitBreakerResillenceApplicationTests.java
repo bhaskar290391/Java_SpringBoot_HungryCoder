0990461dfb11e07e50f1134bf39a7b7cc2e0f7ca
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-05-22 15:25:25 +0530

    resillence4j

---
+++ Bhaskar_Coding/27- Useful-videos/Circuit_breaker_Resillence/src/test/java/com/practise/resillence/CircuitBreakerResillenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.resillence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerResillenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
